7b2ec0ec0ea280ad25b5a57fa14b4724b9c50b2c
Lauren Mastroni <laurenmastroni@gmail.com>
2020-12-22 18:35:46 +0800

    sketch in sections

---
+++ js/animations.js
@@ -0,0 +1,25 @@
+// // Hero fade to yellow on scroll
+//
+// $(function(){
+//     $('.hero').data('size','big');
+// });
+//
+// $(window).scroll(function(){
+//     if($(document).scrollTop() > 50)
+//     {
+//         if($('.hero').data('size') == 'big')
+//         {
+//             $('.hero').data('size','small');
+//             $('.hero').addClass('yellow');
+//
+//         }
+//     }
+//     else
+//     {
+//         if($('.hero').data('size') == 'small')
+//         {
+//             $('.hero').data('size','big');
+//             $('.hero').removeClass('yellow');
+//         }
+//     }
+// });
